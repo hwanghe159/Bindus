34c50044d684eed149b7a9877cbbc12a4868b6d4
stellaeunbyul <stellaspeaksfrench@gmail.com>
2019-07-28 20:38:21 +0900

    sign up page

--- src/app/sign-up2/sign-up2.component.ts
+++ src/app/sign-up2/sign-up2.component.ts
@@ -3,6 +3,7 @@ import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, FormsModule, FormBuilder } from "@angular/forms";
 import * as firebase from "firebase";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-sign-up2',
@@ -10,10 +11,52 @@ import * as firebase from "firebase";
   styleUrls: ['./sign-up2.component.css']
 })
 export class SignUp2Component implements OnInit {
+  errorMessage: string;
+  successMessage: string;
+  userInfo;
+  userBasic;
+  uid: string;
+  email: string;
+  users: Observable<any[]>;
 
-  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) { }
+  constructor(private authService: AuthService, private router: Router, private formBuilder: FormBuilder) {
+    this.userInfo = this.formBuilder.group({
+      name: '',
+      gender: '',
+      birthYear: '',
+      birthMonth: '',
+      birthDate: '',
+      email: '',
+      password: ''
+    })
 
-  ngOnInit() {
+    this.userBasic = this.formBuilder.group({
+      email: '',
+      password: ''
+    })
+
   }
 
+  ngOnInit() {
+
+  }
+
+   tryRegister(value) {
+     this.authService.doRegister(value)
+      .then(res => {
+        console.log(res.uid);
+     
+      }, err => {
+        console.log(err);
+        this.errorMessage = err.message;
+        this.successMessage = "";
+      })
+  }
+  
+ 
+  onSubmit(value) {
+
+     this.tryRegister(value);
+
+  }
 }
