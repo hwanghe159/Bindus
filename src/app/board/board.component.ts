208e39461b33b7bcc76abc09ad10971b792f8934
stellaeunbyul <stellaspeaksfrench@gmail.com>
2019-08-08 17:30:19 +0900

    ngx pagination

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestoreModule, AngularFirestore } from "@angular/fire/firestore";
+import {NgxPaginationModule} from 'ngx-pagination'; 
 
 @Component({
   selector: 'app-board',
@@ -8,31 +9,48 @@ import { AngularFirestoreModule, AngularFirestore } from "@angular/fire/firestor
 })
 export class BoardComponent implements OnInit {
 
-  items =[];
+  config:any;
   pageOfItems: Array<any>;
+  p: number = 1;
+  collection: any[];  
+  items=[];
+  cnt:number;
 
-  constructor(private db: AngularFirestore) { }
+  constructor(private db: AngularFirestore) { 
+
+
+      
+  this.config = {
+    itemsPerPage: 5,
+    currentPage: 1,
+    totalItems: this.cnt
+  };
+  }
 
   ngOnInit() {
+
+
     this.db.collection("brd").get().toPromise().then((querySnapshot) => {
+     this.cnt=querySnapshot.docs.length;
       querySnapshot.forEach((doc) => {
-        this.items.push(doc.data());
+        this.items.push(doc.data());        
         console.log(`${doc.id} => ${doc.data()}`);
       });
     });
-    console.log("POI is "+this.pageOfItems);
 
-    //  this.getData();
     //{id: 1, name: "Item 1"}, {id: 2, name: "Item 2"}...
     // this.getData();
     // this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}` }));
   }
 
+
+  
   onChangePage(pageOfItems: Array<any>) {
     this.pageOfItems = pageOfItems;
   }
 
   getData() {
+
     this.db.collection("brd").get().toPromise().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
         this.items.push(doc.data());
@@ -41,4 +59,7 @@ export class BoardComponent implements OnInit {
     });
 
   }
+
+
+
 }
