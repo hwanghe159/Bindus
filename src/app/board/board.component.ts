28b81a0d7fe2d0c6fff711e40fe71624467d2aff
junho <hwanghe159@naver.com>
2019-08-12 23:54:20 +0900

    Revert "게시판 관련 함수 board.service에 모두 옮김. 상세정보 보여주는 페이지에 UID까지 나옴."
    This reverts commit 9021ed5756010c3d91c251ab8c61cd37c7270170.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestoreModule, AngularFirestore } from "@angular/fire/firestore";
 import { NgxPaginationModule } from 'ngx-pagination';
 import { of } from 'rxjs';
-import { BoardService } from '../board.service';
 
 @Component({
   selector: 'app-board',
@@ -18,31 +17,55 @@ export class BoardComponent implements OnInit {
   items = [];
   cnt: number;
 
-  constructor(private db: AngularFirestore, private boardService: BoardService) {
+  constructor(private db: AngularFirestore) {
+  
+
     this.config = {
       itemsPerPage: 5,
       currentPage: 1,
       totalItems: this.cnt
     };
+
+
   }
 
   ngOnInit() {
-    this.boardService.getData();
+
+    this.getData();
+    
+
+  
 
     //{id: 1, name: "Item 1"}, {id: 2, name: "Item 2"}...
     // this.getData();
     // this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}` }));
   }
 
+
+
+
   pageChanged(event) {
     this.config.currentPage = event;
   }
 
-  // getItems() {
-  //   console.log("this.items = "+this.boardService.items);
-  //   return of(this.boardService.items);
-  // }
 
+  //모든 게시물을 items배열에 저장
+  getData() {
+    this.items = [];
+    this.db.collection("brd").get().toPromise().then((querySnapshot) => {
+      this.cnt = querySnapshot.docs.length;
+      querySnapshot.query.orderBy("brdTime","desc").get().then((data) => {
+        data.forEach((doc=>{
+          this.items.push(doc);
+          }))
+      });
+    });
+  }
+
+  getItems() {
+    console.log("this.items = "+this.items);
+    return of(this.items);
+  }
   // getData() {
 
   //   this.db.collection("brd").get().toPromise().then((querySnapshot) => {
