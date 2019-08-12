a192d03d20fe6e95228cfb45289975ae03845203
junho <hwanghe159@naver.com>
2019-08-13 00:00:40 +0900

    Merge branch 'master' of https://github.com/hwanghe159/Bindus
    # Conflicts:
    #	src/app/detail/detail.component.html
    #	src/app/detail/detail.component.ts

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { BoardComponent } from '../board/board.component';
 import { Observable, of } from 'rxjs';
 import { BoardService } from '../board.service';
 import { FirebaseStorage } from '@angular/fire';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 
 @Component({
@@ -13,22 +15,36 @@ import { FirebaseStorage } from '@angular/fire';
 })
 export class DetailComponent implements OnInit {
 
+  @Input() brdDetails = [];//게시판을 거쳐야만 상세정보가 나올것같음
   brdDetail: any;
-  //picRefs: any[];
+  picUrls: string[] = [];
+  //picRefs: string[];
 
-  constructor(private route: ActivatedRoute, private location: Location, private boardService: BoardService, /*private storage: FirebaseStorage*/) { }
-  //FirebaseStorage 의존성 주입하면 오류남. 왜그런진 모르겠음
+  constructor(private route: ActivatedRoute, 
+    private location: Location, 
+    private boardService: BoardService, 
+    /*private storage: FirebaseStorage*/
+    private storage: AngularFireStorage) { }
   
   ngOnInit() {
     const brdSubId = this.route.snapshot.paramMap.get('id');
     this.boardService.getItem(brdSubId).subscribe(doc => this.brdDetail = doc);
-    //this.getPictures();
+    this.getPictures();
   }
 
   getPictures() {
+    var brdPic1 : string = this.brdDetail.data().brdPic1;
+    var brdPic2 : string = this.brdDetail.data().brdPic2;
+    var brdPic3 : string = this.brdDetail.data().brdPic3;
+    this.picUrls.push(brdPic1);
+    this.picUrls.push(brdPic2);
+    this.picUrls.push(brdPic3);
+
+
     // var httpReference1 = this.storage.refFromURL("{{brdPic1}}");
     // var httpReference2 = this.storage.refFromURL("{{brdPic2}}");
     // var httpReference3 = this.storage.refFromURL("{{brdPic3}}");
+
     // this.picRefs.push(httpReference1);
     // this.picRefs.push(httpReference2);
     // this.picRefs.push(httpReference3);
@@ -37,25 +53,4 @@ export class DetailComponent implements OnInit {
   goBack() {
     this.location.back();
   }
-
-
-  // @Input() brdDetails = [];
-  // brdDetail: any;
-
-  // constructor(private route: ActivatedRoute, private location: Location, private boardService: BoardService) { }
-
-  // ngOnInit() {
-  //   this.boardService.getItems().subscribe(doc => this.brdDetails = doc);
-  //   console.log(this.brdDetails);
-  //   this.getBrdDetail().subscribe(doc => this.brdDetail = doc);
-  // }
-
-  // getBrdDetail() {
-  //   const brdSubId = this.route.snapshot.paramMap.get('id');
-  //   return of(this.brdDetails.find(doc => doc.id.substring(0,6) === brdSubId));
-  // }
-
-  // goBack() {
-  //   this.location.back();
-  // }
 }
