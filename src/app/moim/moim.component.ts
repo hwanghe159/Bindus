d645e5fa3766b5b91487d171fd6748230fab3f40
junho <hwanghe159@naver.com>
2019-08-18 20:38:50 +0900

    모임 상세페이지에서 신청하기버튼 추가

--- src/app/moim/moim.component.ts
+++ src/app/moim/moim.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MoimService } from '../moim.service';
 import { ActivatedRoute } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-moim',
@@ -9,14 +11,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MoimComponent implements OnInit {
 
-  constructor(private moimService: MoimService, private route: ActivatedRoute) { }
+  constructor(private moimService: MoimService, 
+    private route: ActivatedRoute, 
+    /*private firebaseAuth: AngularFireAuth,*/
+    private authService: AuthService ) { }
 
   moim: any;//이 모임에 대한 정보
+  loginStatus: string;
 
   ngOnInit() {
     const subId = this.route.snapshot.paramMap.get('id');
-    this.moimService.getMoimBySubId(subId).subscribe(data => this.moim = data);
-    console.log(this.moim);
+    this.moimService.getMoimBySubId(subId).subscribe(data => this.moim = data); 
   }
 
+
+  async registrate() {
+    let uid = await this.authService.getCurrentUserUID();
+    
+    console.log(uid);
+  }
 }
